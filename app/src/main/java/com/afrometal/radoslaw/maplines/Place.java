460e822da075e5d7dc4ff434b788561ce6234c07
afrometal <radek.kwlsk@gmail.com>
2017-05-18 10:10:33 +0200

    implement Parcelable

--- app/src/main/java/com/afrometal/radoslaw/maplines/Place.java
+++ app/src/main/java/com/afrometal/radoslaw/maplines/Place.java
@@ -1,11 +1,16 @@
 package com.afrometal.radoslaw.maplines;
 
+import android.os.Parcel;
+import android.os.Parcelable;
 import android.support.annotation.NonNull;
 
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
-public class Place implements Comparable {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Place implements Comparable, Parcelable {
 
     @SerializedName("id")
     @Expose
@@ -18,7 +23,10 @@ public class Place implements Comparable {
     private String city;
     @SerializedName("coordinates")
     @Expose
-    private double[] coordinates = null;
+    private List<Double> coordinates = new ArrayList<>();
+
+    public Place() {
+    }
 
     public long getId() {
         return id;
@@ -44,17 +52,17 @@ public class Place implements Comparable {
         this.city = city;
     }
 
-    public double[] getCoordinates() {
+    public List<Double> getCoordinates() {
         return coordinates;
     }
 
-    public void setCoordinates(double[] coordinates) {
+    public void setCoordinates(List<Double> coordinates) {
         this.coordinates = coordinates;
     }
 
     @Override
     public String toString() {
-        return String.format("Place<%d: %s-%s [%f, %f]>", id, country, city, coordinates[0], coordinates[1]);
+        return String.format("Place<%d: %s-%s [%f, %f]>", id, country, city, coordinates.get(0), coordinates.get(1));
     }
 
     @Override
@@ -71,7 +79,7 @@ public class Place implements Comparable {
             return false;
         } else {
             Place oth = (Place) other;
-            if (oth.id == id && oth.country == country && oth.city == city && oth.coordinates[0] == coordinates[0] && oth.coordinates[1] == coordinates[1]) {
+            if (oth.id == id && oth.country.equals(country) && oth.city.equals(city) && oth.coordinates.equals(coordinates)) {
                 return true;
             } else {
                 return false;
@@ -82,5 +90,38 @@ public class Place implements Comparable {
     @Override
     public int compareTo(@NonNull Object o) {
         return city.compareTo(((Place) o).city);
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeLong(id);
+        dest.writeString(country);
+        dest.writeString(city);
+        dest.writeList(coordinates);
+    }
+
+    @SuppressWarnings("unused")
+    public static final Parcelable.Creator<Place> CREATOR = new Parcelable.Creator<Place>() {
+        @Override
+        public Place createFromParcel(Parcel in) {
+            return new Place(in);
+        }
+
+        @Override
+        public Place[] newArray(int size) {
+            return new Place[size];
+        }
+    };
+
+    protected Place(Parcel in) {
+        id = in.readLong();
+        country = in.readString();
+        city = in.readString();
+        coordinates = in.readArrayList(Double.class.getClassLoader());
     }
 }
